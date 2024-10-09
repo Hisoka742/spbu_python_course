965c6e10e6eafb8ec69ad9f9ce6f4721178b6191
Hisoka <atmnaislmane@gmail.com>
2024-10-10 01:05:49 +0300

    fix tests

---
+++ tests/test_cache_decorator.py
@@ -0,0 +1,13 @@
+from curry_uncurry_cache import cache_results
+
+def test_cache_decorator():
+    @cache_results(max_size=2)
+    def add(x, y):
+        return x + y
+
+    assert add(1, 2) == 3
+    assert add(1, 2) == 3  # Should be from cache
+    assert add(2, 3) == 5
+    assert add(3, 4) == 7
+    # Cache should have evicted the (1, 2) result
+    assert add(1, 2) == 3  # Recalculated
---
+++ tests/test_smart_args.py
@@ -0,0 +1,36 @@
+from curry_uncurry_cache import Evaluated, Isolated, smart_args
+
+def test_smart_args_isolated():
+    @smart_args()
+    def test_func(*, d=Isolated()):
+        d['key'] = 'value'
+        return d
+
+    d = {'key': 'initial'}
+    result = test_func(d=d)
+    assert result['key'] == 'value'
+    assert d['key'] == 'initial'  # Original dict is not mutated
+
+def test_smart_args_evaluated():
+    counter = [0]
+
+    def increment_counter():
+        counter[0] += 1
+        return counter[0]
+
+    @smart_args()
+    def test_func(*, value=Evaluated(increment_counter)):
+        return value
+
+    assert test_func() == 1
+    assert test_func() == 2
+    assert test_func() == 3
+
+def test_smart_args_positional():
+    @smart_args(allow_positional=True)
+    def test_func(x=Isolated()):
+        x.append(42)
+        return x
+
+    result = test_func([1, 2, 3])
+    assert result == [1, 2, 3, 42]
