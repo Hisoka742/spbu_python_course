7100d55e05aa8e293c800aa88fc9195a30d5792b
Hisoka <atmnaislmane@gmail.com>
2024-10-22 22:56:01 +0300

    added import

--- project/Game/game.py
+++ project/Game/game.py
@@ -1,5 +1,5 @@
 import random
-
+from Project.Game.game import Wheel, Bot, ConservativeStrategy, AggressiveStrategy, RandomStrategy, Roulette, BotStrategyMeta, GameRulesMeta
 # Metaclass to enforce that any bot strategy class must implement the 'bet' method
 class BotStrategyMeta(type):
     def __new__(cls, name, bases, dct):
