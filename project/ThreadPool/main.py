75dcc2b9defbdc797a74bae72e7a2f8979c36c15
Hisoka <atmnaislmane@gmail.com>
2024-10-25 17:31:43 +0300

    Update main.py

--- project/ThreadPool/main.py
+++ project/ThreadPool/main.py
@@ -10,7 +10,7 @@ class ThreadPool:
 
     def __init__(self, num_threads: int):
         self.num_threads = num_threads
-        self.tasks = []  # List to hold tasks with a lock for thread-safe operations
+        self.tasks: list[Tuple[Callable[[], Any], threading.Event]] = []    # List to hold tasks with a lock for thread-safe operations
         self.lock = threading.Lock()  # Lock for synchronizing access to tasks
         self.task_available = (
             threading.Event()
