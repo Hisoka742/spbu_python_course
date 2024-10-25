4181effee5be189ed11e017a206d003965b17c14
Hisoka <atmnaislmane@gmail.com>
2024-10-25 18:27:37 +0300

    Update strategy.py

--- project/Game/strategy.py
+++ project/Game/strategy.py
@@ -1,27 +1,23 @@
+# strategy.py
 from dataclasses import dataclass
 from typing import Optional
-from abc import ABC, abstractmethod
-
+from abc import abstractmethod
 
 @dataclass
 class Bet:
     """Represents a bet made by a bot."""
-
     type: str
     amount: int
     number: Optional[int] = None
 
-
 class BotStrategyMeta(type):
     """Metaclass that enforces the implementation of the 'bet' method in each strategy."""
-
     def __new__(cls, name, bases, dct):
         if "bet" not in dct:
             raise TypeError(f"Every strategy class must implement 'bet' method.")
         return super().__new__(cls, name, bases, dct)
 
-
-class Strategy(ABC, metaclass=BotStrategyMeta):
+class Strategy(metaclass=BotStrategyMeta):
     """Abstract base class for betting strategies."""
 
     @abstractmethod
